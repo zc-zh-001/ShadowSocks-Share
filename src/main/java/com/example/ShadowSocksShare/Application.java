51464fe976dc445b65558cb9c114da96bb360847
xxx <x@x.x>
2018-03-02 14:56:38 +0800

    Revert "jar -> war"
    This reverts commit 072a8f8

--- src/main/java/com/example/ShadowSocksShare/Application.java
+++ src/main/java/com/example/ShadowSocksShare/Application.java
@@ -2,8 +2,6 @@ package com.example.ShadowSocksShare;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -14,17 +12,10 @@ import java.util.TimeZone;
 @EnableScheduling
 @EnableAsync
 // @EnableCaching
-public class Application extends SpringBootServletInitializer {
+public class Application {
 
-	// ********** jar **********
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-	}
-
-	// ********** war **********
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(Application.class);
 	}
 
 	/**
