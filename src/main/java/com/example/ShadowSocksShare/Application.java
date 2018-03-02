072a8f8726e28a4148438538340232f6c457f989
xxx <x@x.x>
2018-03-02 14:10:51 +0800

    jar -> war

--- src/main/java/com/example/ShadowSocksShare/Application.java
+++ src/main/java/com/example/ShadowSocksShare/Application.java
@@ -2,6 +2,8 @@ package com.example.ShadowSocksShare;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -12,10 +14,17 @@ import java.util.TimeZone;
 @EnableScheduling
 @EnableAsync
 // @EnableCaching
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
+	// ********** jar **********
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	// ********** war **********
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
 	}
 
 	/**
